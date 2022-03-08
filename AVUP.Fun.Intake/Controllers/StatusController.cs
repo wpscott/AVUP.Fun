e64fbfd9f6274669b821383eddb36042939f4389
Scott <wpscott@users.noreply.github.com>
2022-03-08 07:01:33 -0500

    add default gifts

--- AVUP.Fun.Intake/Controllers/StatusController.cs
+++ AVUP.Fun.Intake/Controllers/StatusController.cs
@@ -1,8 +1,13 @@
 ﻿using AcFunDanmu;
+using AcFunDanmu.Models.Client;
 using AVUP.Fun.Intake.Models;
 using AVUP.Fun.Intake.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace AVUP.Fun.Intake.Controllers
@@ -12,9 +17,12 @@ namespace AVUP.Fun.Intake.Controllers
     public class StatusController : Controller
     {
         private readonly ILogger<StatusController> _logger;
+        private readonly IConfiguration _configuration;
 
-        public StatusController(ILogger<StatusController> logger)
+        public StatusController(IConfiguration configuration, ILogger<StatusController> logger)
         {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+
             _logger = logger;
         }
 
@@ -41,13 +49,43 @@ namespace AVUP.Fun.Intake.Controllers
         {
             while (true)
             {
-                if (Client.Gifts.TryGetValue(id, out var gift))
+                var gift = _configuration.GetSection($"Gifts:{id}").Get<Dictionary<string, string>>().ToGiftInfo();
+                if (gift != null)
+                {
+                    return Json(gift);
+                }
+                else if (Client.Gifts.TryGetValue(id, out gift))
                 {
                     return Json(gift);
                 }
                 var client = new Client();
                 await client.Initialize(uper, true);
             }
+        }
+    }
+
+    public static class DictionaryExtension
+    {
+        public static GiftInfo ToGiftInfo(this IDictionary<string, string> source)
+        {
+            if (source == null) { return null; }
+
+            var obj = new GiftInfo();
+            var type = obj.GetType();
+            foreach (var item in source)
+            {
+                var property = type.GetProperty(item.Key, BindingFlags.Public | BindingFlags.IgnoreCase | BindingFlags.Instance);
+                switch (item.Key)
+                {
+                    case "pic":
+                        property.SetValue(obj, new Uri(item.Value)); break;
+                    case "value":
+                        property.SetValue(obj, int.Parse(item.Value)); break;
+                    case "name":
+                        property.SetValue(obj, item.Value); break;
+                }
+            }
+            return obj;
         }
     }
 }
